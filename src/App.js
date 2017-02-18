08fa27a8de73ae8004038148cf63a1fd6694e27a
koofka <jason@primalstare.com>
2017-02-18 13:22:49 -0700

    Project bootstrap and initial component build

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+// Make material-ui happy
+// Needed for onTouchTap
+//http://stackoverflow.com/a/34015469/988941
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+import SearchBox from './SearchBox';
+import makeExpanding from './expanding-animation';
+
+const ExpandingSearchBox = makeExpanding(SearchBox);
+
+class App extends Component {
+  render() {
+    //https://css-tricks.com/quick-css-trick-how-to-center-an-object-exactly-in-the-center/
+    const style = {
+      position: 'fixed',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+    };
+    return (
+      <MuiThemeProvider>
+        <div style={style}>
+          <ExpandingSearchBox />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
